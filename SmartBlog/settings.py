3bde368f0df2573bedc9f92f7dcb22d668e38271
randomwalk <6684536+Randomwak@users.noreply.github.com>
2018-05-18 10:26:22 +0800

    更改setting中数据库配置

--- SmartBlog/settings.py
+++ SmartBlog/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'SmartBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "SmartBlog",
+        'USER':"root",
+        'PASSWORD':"hahahaha",
+        'HOST':'127.0.0.1',
     }
 }
 
