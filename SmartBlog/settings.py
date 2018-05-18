6beac4f6f36ed5db23b67578d65a2a68e852fba1
randomwalk <6684536+Randomwak@users.noreply.github.com>
2018-05-18 10:20:20 +0800

    初始化setting文件，增加模板及静态文件目录

--- SmartBlog/settings.py
+++ SmartBlog/settings.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 """
 Django settings for SmartBlog project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',    #注册blog app
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'SmartBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],  #配置模板文件地址
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,18 +106,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'   #语言设置成中文
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  #时区设置成上海
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False #设置成本地时间
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)  #静态文件目录配置
